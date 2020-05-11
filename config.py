3225311a6f40209fed13fdc4411206db022dcdfa
Bendik Knapstad <knaben@OBOS.NO>
2020-05-11 09:43:50 +0200

    add example config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+
+headers =  {
+
+    "cookie": "Nabohjelp-web-sessions=s%3A2oXpDsfMPXYp7ZNZMMHCo7JolnqQev5D.JZkD4mQiFF8yg1M7nRg0xsi%2F5CQhX2aQdOej1zFfr8k"
+}
+
+cloud_credentials = "obos-225315-4adb95aa0d92.json"
+
+bucket_name = "Your bucket name"
+
+blob_name = "Your blobname"
+
+slack_hook = "Your slack webhook"
+
+microsoft_hook = "Your microsoft webhook"
