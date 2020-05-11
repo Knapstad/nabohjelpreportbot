6f1620d19caabe5b222e8ba056e44f29e8207def
Bendik Knapstad <knaben@OBOS.NO>
2020-05-11 10:22:19 +0200

    added 404 401 handlers

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
 import config
+import requests
 from google.oauth2 import service_account
 from google.cloud import storage
+from myerrors import NotFound, NotAuthenticated
+
 
 SCOPES = ["https://www.googleapis.com/auth/devstorage.read_write"]
 CREDENTIALS = service_account.Credentials.from_service_account_file(
@@ -14,11 +17,22 @@ SLACK_HOOK = config.slack_hook
 
 client = storage.Client(project="obos", credentials=CREDENTIALS)
 
+
 def load_posts_log_from_cloud(
-    client: "google.cloud.storage.client.Client", 
-    blob_name: str, 
-    bucket_name: str
+    client: "google.cloud.storage.client.Client", blob_name: str, bucket_name: str
 ):
     bucket = client.get_bucket(bucket_name)
     blob = bucket.get_blob(blob_name)
     return blob.download_as_string()
+
+
+def get_reported_posts():
+    response = requests.get(config.nabohjelp_api, headers=config.headers)
+    if response.status_code == 200:
+        return response.json(), response.text
+    elif response.status_code == 404:
+        raise NotFound("Page Not Found")
+    elif response.status_code == 401:
+        raise NotAuthenticated("Needs to Log in")
+    else:
+        raise Exception(response.status_code, response.text)
