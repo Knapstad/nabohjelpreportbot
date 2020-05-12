fa136e3718de4bf5fd482d773b1e80389227cda7
Bendik Knapstad <knaben@OBOS.NO>
2020-05-12 12:57:09 +0200

    added save to cloud

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import config
 import requests
+
+from typing import Tuple, Any
 from google.oauth2 import service_account
 from google.cloud import storage
 from myerrors import NotFound, NotAuthenticated
@@ -26,7 +28,20 @@ def load_posts_log_from_cloud(
     return blob.download_as_string()
 
 
-def get_reported_posts():
+def save_posts_to_cloud(
+    client: "google.cloud.storage.client.Client",
+    data: list,
+    blob_name: str,
+    bucket_name: str,
+):
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.get_blob(blob_name)
+    if not blob:
+        blob = bucket.blob(blob_name)
+    blob.upload_from_string(str(data))
+
+
+def get_reported_posts() -> Tuple[Any, str]:
     response = requests.get(config.nabohjelp_api, headers=config.headers)
     if response.status_code == 200:
         return response.json(), response.text
@@ -36,3 +51,7 @@ def get_reported_posts():
         raise NotAuthenticated("Needs to Log in")
     else:
         raise Exception(response.status_code, response.text)
+
+
+def make_slack_message(data: dict) -> str:
+    pass
